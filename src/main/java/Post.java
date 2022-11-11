f6287ec2a2d8e795c8729a4e3de284b00e60e59d
Aleksey Kaschenko <aleksey.kaschenko@gmail.com>
2022-11-11 13:58:12 +0600

    post filled in

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public FormDate birthday;
+    public String name; // поле для имени
+    public String passport;
+    public String patronimic;
+    public String phone;
+    public String surname;
+    public Boolean subscription;
+}
+
